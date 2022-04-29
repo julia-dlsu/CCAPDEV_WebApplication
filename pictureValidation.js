e292dd235bef191ee3f4864dff61ee5234fb03e6
melanieaqui <80889497+melanieaqui@users.noreply.github.com>
2022-04-29 18:28:53 +0800

    Create pictureValidation.js

---
+++ pictureValidation.js
@@ -0,0 +1,17 @@
+//separated this into another file since we might use this outside of Registration
+function validPicture(){
+
+		var picInput = document.Register.pic;
+		var pic = picInput.value;
+		var validType =  /(\.jpg|\.jpeg|\.png)$/i;
+		
+		if(!validType.exec(pic)){
+			alert("Wrong file type. Please input a jpeg, jpg, or png file");
+			picInput.value = '';
+			return false;
+		}
+		else {
+			return true;
+		}
+		
+}
