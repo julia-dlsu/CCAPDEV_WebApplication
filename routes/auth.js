bdb00071f2c72a121fa3bf0213362acdaebc63ad
melanieaqui <80889497+melanieaqui@users.noreply.github.com>
2022-06-14 22:36:32 +0800

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = require('express').Router();
+const userController = require('../controllers/userController');
+const app =express();
+
+//const userController2 = require('../controllers/userController2');
+
+
+
+router.get('/login',(req, res) => {
+  res.render('login')
+});
+router.get('/register',(req, res) => {
+  res.render('register')
+});
+
+
+router.post('/register',userController.registerUser);
+
+router.post('/login',userController.loginUser);
+
+
+// logout
+router.get('/logout', userController.logoutUser);
+
+
+
+
+module.exports = router;
